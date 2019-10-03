21de6f9c330cc725796f707ef0c6a78cdd81cacb
kcraider81 <danny.figura81@gmail.com>
2019-10-03 14:26:25 -0500

    Add files via upload

---
+++ WhatAreWeVotingFor/Controllers/CandidatesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WhatAreWeVotingFor.Models;
+
+namespace WhatAreWeVotingFor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> GetAllCandidates()
+        {
+            var candidate = new Candidate();
+            return JsonConvert.SerializeObject(candidate.AllCandidates());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> GetCandidate(int id)
+        {
+            var candidate = new Candidate();
+            return JsonConvert.SerializeObject(candidate.OneCandidate());
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WhatAreWeVotingFor/Models/Candidate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatAreWeVotingFor.Models
+{
+    [Serializable]
+    public class Candidate
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string State { get; set; }
+        public string Party { get; set; }
+
+        public List<Candidate> AllCandidates()
+        {
+            var candidates = new List<Candidate>();
+            for(int i = 0; i < 10; i++)
+            {
+                candidates.Add(OneCandidate());
+            }
+
+
+            return candidates;
+        }
+
+        public Candidate OneCandidate()
+        {
+            var candidate = new Candidate();
+            Random random = new Random();
+            candidate.FirstName = random.Next(0, 123456789).ToString();
+            candidate.LastName = random.Next(0, 123456789).ToString();
+            candidate.State = random.Next(0, 123456789).ToString();
+            candidate.Party = random.Next(0, 123456789).ToString();
+            return candidate;
+        }
+    }
+}
